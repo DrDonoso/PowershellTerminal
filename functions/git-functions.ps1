9ab43352cce975a670ac49d42a860a95789ec89d
drdonoso <davidrodriguezdonoso95@gmail.com>
2024-09-26 10:13:26 +0200

    Add gitclean to clean local branches from repo

--- functions/git-functions.ps1
+++ functions/git-functions.ps1
@@ -20,3 +20,57 @@ function gitcb() {
     $branch = $branch.Trim()
     git checkout $branch
 }
+
+function gitclean() {
+    git fetch --prune
+
+    $branches = git for-each-ref --format '%(refname:short) %(committerdate:unix)' refs/heads/ |
+        ForEach-Object {
+            $splitResult = $_ -split " "
+            
+            if ($splitResult.Length -eq 2) {
+                $branch = $splitResult[0]
+                $timestamp = $splitResult[1]
+    
+                if ([long]::TryParse($timestamp, [ref]$null)) {
+                    try {
+                        $epoch = [datetime]'1970-01-01T00:00:00Z'
+                        $dateOfCommit = $epoch.AddSeconds([double]$timestamp)
+                        $ageInSeconds = (Get-Date) - $dateOfCommit
+                        $daysSinceCommit = [math]::Floor($ageInSeconds.TotalDays)
+    
+                        return [PSCustomObject]@{
+                            Branch = $branch
+                            DaysSinceCommit = $daysSinceCommit
+                        }
+                    } catch {
+                        Write-Host "Error calculating the age of branch '$branch'. Error: $_"
+                    }
+                } else {
+                    Write-Host "Error: Timestamp is not a valid number. Branch: '$branch', Timestamp: '$timestamp'"
+                }
+            }
+        }
+
+    $deletedCount = 0
+    foreach ($branchInfo in $branches) {
+        $branch = $branchInfo.Branch
+        $daysSinceCommit = $branchInfo.DaysSinceCommit
+
+        if ($branch -notin @('main', 'master', 'develop')) {
+            Write-Host -ForegroundColor Yellow -NoNewline "⚠️ Do you want to delete the branch '$branch' (last commit $daysSinceCommit days ago)? (Y/n) "
+            $confirmation = Read-Host
+            if ($confirmation -eq '' -or $confirmation -eq 'y' -or $confirmation -eq 'Y') {
+                git branch -D $branch
+                Write-Host "🗑️ Deleting branch: $branch"
+                $deletedCount++
+            } else {
+                Write-Host "Did not delete the branch: $branch"
+            }
+        } else {
+            Write-Host "Skipping protected branch: $branch"
+        }
+    }
+
+    Write-Host "Deleted $deletedCount branches."
+}
